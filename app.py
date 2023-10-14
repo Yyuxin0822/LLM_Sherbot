1991e9e99cde951e3bbfc8932030ed12029da69c
Ubuntu <yyuxin0822@gmail.com>
2023-10-14 17:25:11 +0000

    Sherbot_v2

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from Sherbot_v1 import getcanvas,getinput,getoutput,getinputlist,getoutputlist,flowdict,categorize,color
-from flask import Flask, render_template, redirect, url_for, request
+from Sherbot import simpleflow_main,complexflow_main,getcanvas_main,inspirationflow_main,inquiresys_main,logtoJson
+from flask import Flask, render_template, redirect, url_for,jsonify,request
 from form import EnvDescriptionForm
 import json
+from datetime import datetime
 
 app=Flask(__name__,static_folder="static", template_folder='templates')
 app.config["SECRET_KEY"] = "mysecret"
 
-canvas={0:""}
 envD={0:""}
+canvas={0:""}
 resultD={0:""}
+flowD={0:""}
+systemD={0:""}
 colorD={0:""}
+matrixD={0:""}
 
 @app.route('/', methods=['GET','POST'])
 def index(id=0):
@@ -18,15 +22,48 @@ def index(id=0):
         env=env_form.env_description.data
         id=len(canvas)
         envD[id]=env
-        result=flowdict(getoutput(env))
-        resultD[id]=result
-        colorD[id]= color(categorize(result))
-        #inputD[id]=getinputlist(result)
-        #outputD[id]=getoutputlist(result)
-        canvas[id]=getcanvas(env)
+
+        #getcanvasD
+        canvas[id]=getcanvas_main(env)
+        simpleflow_main(env)
+
         return redirect(url_for('index',id=id))
     id=len(canvas)-1
-    return render_template("index.html",env=envD[id],result=resultD[id],canvas=canvas[id],color=colorD[id],temp_form=env_form)
+    return render_template("index.html",env=envD[id],canvas=canvas[id],temp_form=env_form)
+
+@app.route('/inquire_data', methods=['POST'])
+def inquire_data():
+    data = request.get_json()
+    # convert data to dictionary
+    # result=json.load(data)
+    result_flow=data["toinquireFlow"]
+    result_element=data["toinquireElement"]
+    result_system=data["toinquireSystem"]
+    
+    messagefilepath="static/message/sherbotmessage.json"
+    now = datetime.now()
+    formatted_time = now.strftime("%H:%M:%S")
+
+
+    logtoJson("--------(Time of This Ask:"+formatted_time+")--------- \n",messagefilepath)
+    logtoJson("--------(Time of This Ask:"+formatted_time+")--------- \n",messagefilepath)
+    logtoJson("Hi! I am Sherbot! \n",messagefilepath)
+
+    if result_flow:
+        complexflow_main(result_flow)
+    if result_element:
+        inspirationflow_main(result_element)
+    if result_system:
+        inquiresys_main(result_system)
+    return jsonify(data)
+
+@app.route('/regenerate_image', methods=['POST'])
+def regenerate_image():
+    data=request.get_json()
+    if data["image"]:
+        url=getcanvas_main(data["image"])
+    dicturl={"url":url}
+    return jsonify(dicturl)
 
 if __name__=="__main__":
     app.run(debug=True)
--- form.py
+++ form.py
@@ -3,5 +3,8 @@ from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 class EnvDescriptionForm(FlaskForm):
-    env_description= TextAreaField("env_description",validators=[DataRequired()])
-    submit=SubmitField("Generate Flow")
+    env_description= TextAreaField("env_description",validators=[DataRequired()], render_kw={"placeholder": "Hey! Please try an environment description here..."})
+    submit=SubmitField("GENERATE FLOW")
+
+
+
